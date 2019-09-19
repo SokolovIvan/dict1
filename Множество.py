e0bbadfba808bea033df203547d71c8680cbc4e6
SokolovIvan <55226826+SokolovIvan@users.noreply.github.com>
2019-09-19 09:46:28 +0500

    Delete Множество.py

--- Множество.py
+++
@@ -1,7 +0,0 @@
-list1 = list(map(int, input().split()))
-list2 = list(map(int, input().split()))
-set1 = set(list1)
-set2 = set(list2)
-set3 = [set1 & set2]
-for elem in set3:
-    print(*sorted(elem, reverse=False))
