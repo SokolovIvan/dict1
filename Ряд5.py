746e77f67b3f0fb1d3bd12b318660486b40a75fb
SokolovIvan <55226826+SokolovIvan@users.noreply.github.com>
2020-09-27 22:27:34 +0500

    Delete Ряд5.py

--- Ряд5.py
+++
@@ -1,9 +0,0 @@
-intList = list(map(int, input().split()))
-intList1 = []
-n = len(intList)
-m = 0
-while m < n:
-    if intList[m] % 2 == 0:
-        intList1.append(intList[m])
-    m += 1
-print(*intList1)
