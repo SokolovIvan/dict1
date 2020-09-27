bc28294552319748f342671644a8dc6f655cb690
SokolovIvan <55226826+SokolovIvan@users.noreply.github.com>
2020-09-27 22:28:11 +0500

    Delete Ряд8.py

--- Ряд8.py
+++
@@ -1,9 +0,0 @@
-intList = list(map(int, input().split()))
-intList1 = []
-n = len(intList)
-m = 1
-while m < n:
-    if intList[m] > intList[m-1]:
-        intList1.append(intList[m])
-    m += 1
-print(*intList1)
